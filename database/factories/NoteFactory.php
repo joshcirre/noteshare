ad4e7c8800429f6940341665e7e055825084e9b0
Josh Cirre <joshcirre@gmail.com>
2023-12-01 03:53:41 -0700

    note seeder

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'user_id' => User::factory(),
+            'subject' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'recipient' => $this->faker->email,
+            'published' => $this->faker->boolean,
+            'send_date' => $this->faker->dateTimeBetween('now', '+30 days')->format('Y-m-d'),
+            'heart_count' => $this->faker->numberBetween(0, 100),
+        ];
+    }
+}
---
+++ database/seeders/NoteSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class NoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::all()->each(function ($user) {
+            Note::factory(10)->create(['user_id' => $user->id]);
+        });
+    }
+}
